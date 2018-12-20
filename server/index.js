0afe17d717df6e903b3d7040cee20c78a199faa6
Araf Hossain <arafh13@gmail.com>
2018-12-20 16:26:31 -0500

    Added get/Reviews handler to send database info

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const retrieve = require('../database/index').retrieve;
 
 let app = express();
 app.use(express.static(__dirname + '/../client/dist'));
@@ -8,6 +9,16 @@ app.use(bodyParser.json());
 /*
 Add functionality here. . . . . 
 */
+
+app.get('/reviews', (req, res) => {
+console.log('reviews');
+retrieve()
+.then((data) => {
+    res.send(data)
+})
+.catch((err) => {
+    res.sendStatus(500)})
+});
 
 let port = 3007;
 
