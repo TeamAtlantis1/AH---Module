b11f6ad1d4f9317c621129e102ed3978c35c085d
Araf Hossain <arafh13@gmail.com>
2018-12-20 09:23:36 -0500

    Created seedFile and function to insert mockdata into db

--- database/insertAllListings.js
+++ database/insertAllListings.js
@@ -1,10 +1,16 @@
-var mockData = require ('./MOCK_DATA');
+var mockData = require ('./MOCK_DATA').mockData;
 var db = require('./connection');
 var mongoose = require('mongoose');
-var schema = require('./index');
+var Listing = require('./index').Listing;
 
 var insertAllListings = function() {
-  schema.collection.insertMany(mockData, function(err, r) {
+  console.log(mockData[0]);
+  Listing.insertMany(mockData, function(err, r) {
+    if(err){
+      console.log('Error!', err);
+    } else {
+      console.log('Success!', r)
+    }
     db.close();
   });
 };
